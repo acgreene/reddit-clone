629eb474f0530f984425eb5825fa800176a58be9
acgreene <aleccgreene@gmail.com>
2023-03-21 20:18:22 -0400

    added google authentication.

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,13 +1,22 @@
-import { Button, Flex, Image } from "@chakra-ui/react";
+import { Button, Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth } from "@/src/firebase/clientApp";
 
 const OAuthButtons: React.FC = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
   return (
     <Flex direction="column" width="100%" mb={4}>
-      <Button variant="oauth" mb={2}>
+      <Button
+        variant="oauth"
+        mb={2}
+        isLoading={loading}
+        onClick={() => signInWithGoogle()}
+      >
         <Image src="images/googlelogo.png" height="20px" mr={4} />
         Continue with Google
       </Button>
+      {error && <Text>{error.message}</Text>}
     </Flex>
   );
 };
